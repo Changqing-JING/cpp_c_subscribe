88ae6b2e7dffafa36f9ad765ab2f8dafd1bd2336
Changqing Jing <changqing.jing@bmw.com>
2021-05-24 19:07:55 +0800

    fix register usage bug

--- asm/bridge_arm.s
+++ asm/bridge_arm.s
@@ -6,13 +6,13 @@
 	.type	main, %function
 main:
 .LFB0:
-	    mov     x0, 30600
-        movk    x0, 0x5566, lsl 16
-        movk    x0, 0x3344, lsl 32
-        movk    x0, 0x1122, lsl 48
-		ldr     x15, [x0, 8]
-        ldr     x0, [x0]
-        br      x0
+	    mov     x8, 30600
+        movk    x8, 0x5566, lsl 16
+        movk    x8, 0x3344, lsl 32
+        movk    x8, 0x1122, lsl 48
+		ldr     x15, [x8, 8]
+        ldr     x8, [x8]
+        br      x8
 .LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
